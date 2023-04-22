cbb1ce8605107a73942434547d731dcfbaf067ed
Jacky Xie <gokurohono@gmail.com>
2023-04-23 00:17:22 +1000

    feat(frontend): minor itemContainer UI tweaks

--- frontend/src/components/kanbanBoard/ItemContainer.tsx
+++ frontend/src/components/kanbanBoard/ItemContainer.tsx
@@ -6,10 +6,16 @@ type Props = {
 }
 
 const Container = styled.div`
-  border: 1px solid black;
+  border: 0.5px solid grey;
   margin: 10px;
+  padding: 20px 10px;
   border-radius: 5px;
   height: 30px;
+
+  &:hover {
+    background: grey;
+    color: white;
+  }
 `
 
 export default function ItemContainer({ title }: Props) {
