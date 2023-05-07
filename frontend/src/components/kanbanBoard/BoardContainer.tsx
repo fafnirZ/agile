480766ee087e5e682e785e9c768cc1c955b3c36a
Jacky Xie <gokurohono@gmail.com>
2023-05-08 00:47:47 +1000

    feat(dnd): overall dnd is working and state saves

--- frontend/src/components/kanbanBoard/BoardContainer.tsx
+++ frontend/src/components/kanbanBoard/BoardContainer.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import ListContainer from './ListContainer'
-import { DragDropContext, Droppable } from "@hello-pangea/dnd";
+import { DragDropContext, DropResult } from "@hello-pangea/dnd";
 import styled from 'styled-components'
 import { ListsMock } from '../../mock/list';
 import { insert, reorder } from '../../utils/arrays';
@@ -18,7 +18,6 @@ const Container = styled.div`
 function BoardContainer({}: Props) {
   const [lists, setLists] = useState<ListType[]>([])
 
-
   // onMount
   useEffect(() => {
     // use mock data
@@ -27,14 +26,14 @@ function BoardContainer({}: Props) {
     // TODO
   },[])
 
-  const onDragEnd = (result: any) => {
+  const onDragEnd = (result: DropResult) => {
     // console.log(result)
     const src = result.source
     const dest = result.destination
+    if (!dest) return;
+
     const src_list = lists.filter(l => l.id == src.droppableId)[0].items
     const dest_list = lists.filter(l => l.id == dest.droppableId)[0].items
-
-    if (!dest) return;
     if (src.droppableId == dest.droppableId && src.index == dest.index) return;
     if (src.droppableId == dest.droppableId && src.index != dest.index) {
       // intra-list movement
@@ -46,30 +45,28 @@ function BoardContainer({}: Props) {
         }
         return list
       }))
-      console.log(lists)
       return;
     }
     else {
       // inter-list movement
+
       // remove item from source from index
-      const [ old_item ] = src_list.splice(src.index)
-      // const new_src_list = [...src_list.splice(0, src.index), ...src_list.splice(src.index+1)]
-      console.log(old_item)
-      // // add item to destination in index
+      // const [ old_item ] = src_list.splice(src.index)
+      const new_src_list = [...src_list.slice(0, src.index), ...src_list.slice(src.index+1)]
+      const old_item = src_list[src.index]
+      // add item to destination in index
       const new_dest_list = insert<ItemType>(dest_list, dest.index, old_item)
 
-      // // save
+      // save
       setLists(prevState => prevState.map(list => {
         if (list.id == src.droppableId) {
-          list.items = src_list; return list;
+          list.items = new_src_list; return list;
         } else if (list.id == dest.droppableId) {
           list.items = new_dest_list; return list;
         } else {
           return list;
         }
       }))
-
-      console.log(lists)
     }
 
   }
--- frontend/src/utils/arrays.ts
+++ frontend/src/utils/arrays.ts
@@ -10,6 +10,7 @@ export const insert = <T>(arr: T[], index: number, newItem: T) => [
 export const reorder = <T>(list: T[], startIndex: number, endIndex: number) => {
   const result = Array.from(list);
   const [removed] = result.splice(startIndex, 1);
+  console.log(result)
   result.splice(endIndex, 0, removed);
   return result;
 };
